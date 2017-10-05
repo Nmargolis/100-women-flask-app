dfbceeafe084a9fa9fff852ca3654cfddb80e73d
David Skip Everling <david.everling@gmail.com>
2017-10-05 05:50:57 -0700

    comment out old upload_file route

--- server.py
+++ server.py
@@ -23,21 +23,21 @@ ALLOWED_EXTENSIONS = set(['wav','m4a'])
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
 
-@app.route('/upload')
+@app.route('/upload', methods=['GET', 'POST'])
 def process_speech():
     #get request audio file
     #wav_file = request.body.get(‘content’)
     wav_file = "test.wav" #for testing
 
-    watson_json= transcibe_watson(wav_file)
+    watson_json= transcribe_watson(wav_file)
 
     df = makeDFfromJson(watson_json)
 
     speaker_dict = retreiveSpeakerInfoAsDict(df)
     for speaker in speaker_dict:
-        speaker["name"]=get_name_from_first_sentence(sentences)
+        speaker["name"] = get_name_from_first_sentence(sentences)
         if speaker["name"]==None:
-               speaker["name"]==speaker
+               speaker["name"] = speaker #default to speaker's ID number
         speaker["top_cats"] = get_semantic_categories(sentences)
 
     #save speaker_dict to file
@@ -47,7 +47,7 @@ def process_speech():
     return jsonify(speaker_dict=speaker_dict)
 
 
-@app.route('/names')
+@app.route('/names', methods=['GET'])
 def get_names():
     names = defaultdict(str)
     with open('speaker_dict.json', 'r') as fp:
@@ -57,7 +57,7 @@ def get_names():
         # OR names[speaker["name"]] = speaker    # e.g. {Erin:"0"}
     return jsonify(names=names)
 
-@app.route('/results')
+@app.route('/results', methods=['GET'])
 def get_results():
     with open('speaker_dict.json', 'r') as fp:
         results = json.loads(fp)
@@ -66,8 +66,6 @@ def get_results():
 # Set "homepage" to index.html
 @app.route('/')
 def index():
-    print (nlp.to_spacy_doc())
-    print (nlp.to_textacy_doc())
     return render_template('index.html')
 
 # Save e-mail to database and send to success page
@@ -90,40 +88,42 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route('/', methods=['GET', 'POST'])
-def upload_file():
-    if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        # if user does not select file, browser also
-        # submit a empty part without filename
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 
-        r = sr.Recognizer()
-        if filename.rsplit('.', 1)[1].lower() == 'm4a':
-            m4_audio = AudioSegment.from_file(app.config['UPLOAD_FOLDER']+filename, format="m4a")
-            m4_audio.export("sound_file.wav", format="wav")
-            filename = "sound_file.wav"
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            with sr.WavFile(filename) as source:
-                    audio = r.record(source)
-        else:
-            with sr.WavFile(app.config['UPLOAD_FOLDER']+filename) as source:
-                    audio = r.record(source)
-
-        try:
-            flash("Transcription: " + r.recognize_google(audio))
-        except LookupError:
-            flash("Could not understand audio")
-    return render_template('index.html')
+### Note: requires SpeechRecognition package
+# @app.route('/upload_file', methods=['GET', 'POST'])
+# def upload_file():
+#     if request.method == 'POST':
+#         # check if the post request has the file part
+#         if 'file' not in request.files:
+#             flash('No file part')
+#             return redirect(request.url)
+#         file = request.files['file']
+#         # if user does not select file, browser also
+#         # submit a empty part without filename
+#         if file.filename == '':
+#             flash('No selected file')
+#             return redirect(request.url)
+#         if file and allowed_file(file.filename):
+#             filename = secure_filename(file.filename)
+#             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+#
+#         r = sr.Recognizer()
+#         if filename.rsplit('.', 1)[1].lower() == 'm4a':
+#             m4_audio = AudioSegment.from_file(app.config['UPLOAD_FOLDER']+filename, format="m4a")
+#             m4_audio.export("sound_file.wav", format="wav")
+#             filename = "sound_file.wav"
+#             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+#             with sr.WavFile(filename) as source:
+#                     audio = r.record(source)
+#         else:
+#             with sr.WavFile(app.config['UPLOAD_FOLDER']+filename) as source:
+#                     audio = r.record(source)
+#
+#         try:
+#             flash("Transcription: " + r.recognize_google(audio))
+#         except LookupError:
+#             flash("Could not understand audio")
+#     return render_template('index.html')
 
 
 if __name__ == '__main__':
